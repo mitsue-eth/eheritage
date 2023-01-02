1f4c58683b27069dfd5fbdf2abdb52c47d0254a5
Mitsue <mitsue.eth@gmail.com>
2023-01-02 09:14:37 +0100

    added stars

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 // import Image from "next/image";
 import styles from "./page.module.scss";
 import Lock from "../components/Logo/lock";
+import Stars from "../components/Stars/stars";
 import Image from "next/image";
 import LockImage from "../public/Logo/logo-time-clock.svg";
 // import styless from "./page.module.scss";
@@ -8,7 +9,7 @@ import LockImage from "../public/Logo/logo-time-clock.svg";
 export default function Home() {
   return (
     <div className={styles.container}>
-      <div className={styles.stars}></div>
+      <Stars />
       <main className={styles.main}>
         <div className="max-w-xs w-60">
           <Lock />
---
+++ components/Stars/stars.tsx
@@ -0,0 +1,9 @@
+import styles from "./stars.module.scss";
+
+export default function Stars() {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.starA} />;
+    </div>
+  );
+}
